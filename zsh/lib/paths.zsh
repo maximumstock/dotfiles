44a83e184caf4b3fe48c3602f1a6476b2f16035c
Maximilian Stock <me@maximumstock.net>
2018-01-04 13:15:11 +0100

    Fix zshenv sourcing

--- zsh/lib/paths.zsh
+++
@@ -1,7 +0,0 @@
-# Contains all custom path variables
-########################################################################
-
-# Postgres.app
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
-# Java
-export JAVA_HOME=$(/usr/libexec/java_home)
