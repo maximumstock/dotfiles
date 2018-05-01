e6c753cbfc29b1031e2aa5db5b00d3a82aa6bb55
Maximilian Stock <me@maximumstock.net>
2018-05-01 13:41:54 +0200

    Fix zsh del character encoding; Stop sharing session histories for multiple tmux sessions/buffers; Add new spacemacs config

--- zsh/lib/history.zsh
+++ zsh/lib/history.zsh
@@ -10,4 +10,4 @@ setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
 setopt hist_verify
-setopt share_history # share command history data
+# setopt share_history # share command history data
--- zsh/lib/hotkeys.zsh
+++ zsh/lib/hotkeys.zsh
@@ -1,2 +1,3 @@
 bindkey "^[f" forward-word
 bindkey "^[b" backward-word
+bindkey "^[[3~" delete-char
