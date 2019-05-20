9fcdbb19638a732a5c22f93c418e56598a89f03c
Maximilian Stock <me@maximumstock.net>
2019-05-20 19:55:02 +0200

    fix(): Tmux 2.9 style options

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -21,36 +21,32 @@ set -g status-interval 5
 # set-option -g status-bg colour0
 set-option -g status-fg $tm_color_active
 set-option -g status-bg default
-set-option -g status-attr default
 
 # default window title colors
-set-window-option -g window-status-fg $tm_color_inactive
-set-window-option -g window-status-bg default
+set -g window-status-style fg=$tm_color_inactive,bg=default
 set -g window-status-format "#I #W"
 
 # active window title colors
-set-window-option -g window-status-current-fg $tm_color_active
-set-window-option -g window-status-current-bg default
-set-window-option -g window-status-current-format "#[bold]#I #W"
+set -g window-status-current-style fg=$tm_color_active,bg=default
+set -g window-status-current-format "#[bold]#I #W"
 
 # pane border
-set-option -g pane-border-fg $tm_color_inactive
-set-option -g pane-active-border-fg $tm_active_border_color
+set -g pane-border-style fg=$tm_color_inactive
+set -g pane-active-border-style fg=$tm_color_inactive
 
 # message text
-set-option -g message-bg default
-set-option -g message-fg $tm_color_active
+set -g message-style fg=$tm_color_active,bg=default
 
 # pane number display
-set-option -g display-panes-active-colour $tm_color_active
-set-option -g display-panes-colour $tm_color_inactive
+set -g display-panes-active-colour $tm_color_active
+set -g display-panes-colour $tm_color_inactive
 
 # clock
 set-window-option -g clock-mode-colour $tm_color_active
 
 tm_date="#[fg=$tm_color_inactive] %R %d %b"
 tm_host="#[fg=$tm_color_feature,bold]#h"
-tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon #S"
+tm_session_name="#[fg=$tm_color_feature,bold]#S"
 
 set -g status-left $tm_session_name' '
 set -g status-right $tm_date' '$tm_host
