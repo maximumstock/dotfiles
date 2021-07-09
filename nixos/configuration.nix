4a5ca484336890888acc02287691799f6c4e09ec
Maximilian Stock <mxmlnstock@googlemail.com>
2021-07-09 19:26:52 +0200

    feat: Add NixOS config

---
+++ nixos/configuration.nix
@@ -0,0 +1,181 @@
+# Edit this configuration file to define what should be installed onC
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nixpkgs.config = {
+    packageOverrides = pkgs: rec {
+      polybar = pkgs.polybar.override {
+        i3GapsSupport = true;
+        pulseSupport = true;
+        mpdSupport = true;
+        nlSupport = true;
+      };
+    };
+  };
+
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.useOSProber = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  networking.hostName = "anaconda"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp34s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  
+  fonts = {
+    enableDefaultFonts = true;
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [ "DejaVu Sans Mono" ];
+        serif = [ "DejaVu Sans" ];
+        sansSerif = [ "DejaVu Sans Serif" ];
+      };
+    };
+    fonts = with pkgs; [
+      noto-fonts
+      hack-font
+      font-awesome-ttf
+      powerline-fonts
+    ];
+  };
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "de_DE.UTF-8";
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+
+  # Enable the GNOME 3 Desktop Environment.
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  
+  services.xserver.desktopManager.xterm.enable = false;
+  services.xserver.displayManager.defaultSession = "none+i3";
+
+  services.xserver.windowManager.i3 = {
+    enable = true;
+    package = pkgs.i3-gaps;
+    extraPackages = with pkgs; [
+      i3status
+      i3lock
+      polybar
+    ];
+  };
+
+  # MPD
+  services.mpd = {
+    enable = true;
+    extraConfig = ''
+      audio_output {
+        type "pulse" # MPD must use Pulseaudio
+        name "Pulseaudio" # Whatever you want
+        server "127.0.0.1" # MPD must connect to the local sound server
+      }
+    '';
+  };
+
+  # Configure keymap in X11
+  services.xserver.layout = "de";
+  services.xserver.xkbOptions = "eurosign:e, ctrl:nocaps";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.package = pkgs.pulseaudioFull;
+  hardware.pulseaudio.extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1"; # Needed by mpd to be able to use Pulseaudio
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.maximumstock = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    firefox
+    libreoffice
+    vlc
+    parted
+    alacritty
+    neovim
+    git
+    htop
+    tmux
+    tree
+    zsh
+    fzf
+    curl
+    wget
+    ripgrep
+  ];
+
+  environment.variables = {
+    EDITOR = pkgs.lib.mkOverride 0 "nvim";
+    SHELL = pkgs.lib.mkOverride 0 "zsh";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  virtualisation.docker.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+}
+
